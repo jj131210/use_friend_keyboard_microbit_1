0e7de5486a490c3913662808dcde059da5a4a8cd
jj <164000317+jj131210@users.noreply.github.com>
2024-08-26 21:31:59 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(1)
 /**
  * 이 코드는 송신부 
  * 
  * 입니다
  */
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0)
-})
-radio.setGroup(1)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
